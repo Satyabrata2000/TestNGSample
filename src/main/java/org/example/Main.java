0e3abdabe8be17be0524e9581c5f132a25d9e708
Satyabrata2000 <satyabrata2000@gmail.com>
2023-01-11 04:57:27 -0600

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,22 @@ import org.testng.annotations.Test;
 public class Main {
 
     @Test
-    public void test(){
-        System.out.println("Welcome");
+    public void test1(){
+        System.out.println("Line 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Line 2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Line 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("Line 4");
     }
 
 }
